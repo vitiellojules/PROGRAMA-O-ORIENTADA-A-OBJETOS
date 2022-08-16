beac9ff6b93834a66415381fcdb43afaa4b4734e
vitiellojules <110860784+vitiellojules@users.noreply.github.com>
2022-08-15 22:30:08 -0300

    Add files via upload

---
+++ TesteJava.java
@@ -0,0 +1,16 @@
+package ifsc;
+
+public class TesteJava {
+public static void main(String[]args, String StringVar) {
+	
+	StringVar = " 654554"+6545454;
+	
+	System.out.println(StringVar);
+	 
+	
+
+	
+	
+}
+
+}
